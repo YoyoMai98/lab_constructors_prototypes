0b59c5119e6eaaf3035fbad279a1c1b5be2b3661
Yongran <maiyongran@gmail.com>
2022-09-22 15:41:27 +0100

    add methods and tests for Dealership

--- task.test.js
+++ task.test.js
@@ -11,3 +11,27 @@ test('can access to getPrice', () => {
 test('can access to getEngineTyoe', () => {
     expect(task.car1.getEngineType()).toEqual("N46");
 })
+
+test('can access to getDealshipName', () => {
+    expect(task.dealership1.getDealerName()).toEqual("Cargiant");
+})
+
+test('can get max number of cars', () => {
+    expect(task.dealership1.getMaxNumberOfCars()).toEqual(10);
+})
+
+test('can count number of cars', () => {
+    expect(task.dealership1.countNumberOfCars()).toEqual(1);
+})
+
+test('can return manufacturers for each car', () => {
+    expect(task.dealership1.getAllManufacturers()).toEqual(["BMW"]);
+})
+
+test('can get all the cars from a given manufacturer', () => {
+    expect(task.dealership1.findAGivenManufacturer("BMW").length).toEqual(1);
+})
+
+test('can find the total value of all the cars in stock', () => {
+    expect(task.dealership1.countTotalValue()).toEqual(38);
+})
--- task_constructors_prototypes.js
+++ task_constructors_prototypes.js
@@ -11,6 +11,34 @@ Car.prototype.setPrice = function(price){this.price = price};
 Car.prototype.getEngineType = function(){return this.engineType};
 Car.prototype.setEngineType = function(engineType){this.engineType = engineType};
 
+const Dealership = function(dealerName, maxNumberOfCars){
+    this.dealerName = dealerName;
+    this.maxNumberOfCars = maxNumberOfCars;
+    this.cars = [];
+}
+
+Dealership.prototype.getDealerName = function(){return this.dealerName};
+Dealership.prototype.setDealerName = function(dealerName){this.dealerName = dealerName};
+Dealership.prototype.getMaxNumberOfCars = function(){return this.maxNumberOfCars};
+Dealership.prototype.setMaxNumberOfCars = function(maxNumberOfCars){this.maxNumberOfCars = maxNumberOfCars};
+Dealership.prototype.getCars = function(){return this.cars};
+Dealership.prototype.addCars = function(car){this.cars.push(car)};
+Dealership.prototype.removeCars = function(car){this.cars.splice(this.cars.indexOf(car),1)};
+
+Dealership.prototype.countNumberOfCars = function(){return this.cars.length};
+Dealership.prototype.getAllManufacturers = function(){
+    return this.cars.map(car => car.getManufacturer());
+}
+Dealership.prototype.findAGivenManufacturer = function(manufacturer){
+    return this.cars.filter(car => car.getManufacturer() === manufacturer);
+}
+Dealership.prototype.countTotalValue = function(){
+    return this.cars.reduce((reducer, car) => reducer + car.getPrice(), 0)
+}
+
 const car1 = new Car('BMW', 38, 'N46');
 
-module.exports = {car1};
+const dealership1 = new Dealership('Cargiant', 10);
+dealership1.addCars(car1);
+console.log(dealership1.countTotalValue());
+module.exports = {car1, dealership1};
